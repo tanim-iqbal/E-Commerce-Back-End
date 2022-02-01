e8d813b8f451df175f88a3f15cd71c2cd3988d8e
NY4TI <tanim.iqbal13@gmail.com>
2022-01-31 22:43:22 -0500

    updated code

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,6 +7,36 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+        
+
+      },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      },
+    },
+  },
   },
   {
     sequelize,
@@ -18,3 +48,13 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+
+// CREATE TABLE  productTag (
+//   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+//   product_id INT,
+//   FOREIGN KEY (product_id) REFERENCES product(id)
+//   ON DELETE SET NULL
+//   tag_id INT,
+//   FOREIGN KEY (tag_id) REFERENCES tag(id) ON DELETE SET NULL,
+// );
